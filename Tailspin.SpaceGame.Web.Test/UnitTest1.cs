a842810e96dad944e0917accfa353a10ae71fe27
Jasper Dirix <jasper.dirix@student.pxl.be>
2023-01-25 18:50:15 +0100

    Create TestProject

---
+++ Tailspin.SpaceGame.Web.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Tailspin.SpaceGame.Web.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
